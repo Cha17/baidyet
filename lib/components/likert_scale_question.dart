5c9710dc0d689d0c405e7ee5826d250a429d90b2
Charl Cortez <charlcrtz@gmail.com>
2024-10-15 20:10:55 +0800

    updated questionnaire

--- lib/components/likert_scale_question.dart
+++ lib/components/likert_scale_question.dart
@@ -4,62 +4,98 @@ class LikertScaleQuestion extends StatelessWidget {
   final String question;
   final int? selectedValue;
   final Function(int) onSelect;
+  final bool isEnabled;
+  final double opacity;
 
   const LikertScaleQuestion({
     Key? key,
     required this.question,
     required this.selectedValue,
     required this.onSelect,
+    required this.isEnabled,
+    required this.opacity,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(
-          question,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: List.generate(7, (index) {
-            return GestureDetector(
-              onTap: () => onSelect(index),
-              child: Container(
-                width: 24,
-                height: 24,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: selectedValue == index
-                      ? (index < 3
-                          ? Colors.green
-                          : index > 3
-                              ? Colors.red
-                              : Colors.grey)
-                      : Colors.white,
-                  border: Border.all(color: Colors.grey),
+    return Opacity(
+      opacity: opacity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            question,
+            style: TextStyle(fontSize: 14, color: Colors.grey[800]),
+            textAlign: TextAlign.center,
+            // overflow: TextOverflow.ellipsis,
+            maxLines: 3,
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: List.generate(7, (index) {
+              // Calculate size based on distance from center
+              double size = 40 - (3 - (index - 3).abs()) * 4;
+
+              return GestureDetector(
+                onTap: isEnabled ? () => onSelect(index) : null,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: size,
+                      height: size,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: selectedValue == index
+                            ? (index < 3
+                                ? const Color(0xFF37843A)
+                                : index > 3
+                                    ? const Color(0xFFA32E25)
+                                    : Colors.grey)
+                            : Colors.white,
+                        border: Border.all(color: const Color(0xFFA3E4D7)),
+                      ),
+                    ),
+                    if (selectedValue == index)
+                      Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: size * 0.6,
+                      ),
+                  ],
                 ),
-              ),
-            );
-          }),
-        ),
-        const SizedBox(height: 8),
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Agree', style: TextStyle(color: Colors.green)),
-            Text('Disagree', style: TextStyle(color: Colors.red)),
-          ],
-        ),
-        const SizedBox(height: 16),
-        Container(
-          height: 1,
-          color: Colors.grey[300],
-        ),
-      ],
+              );
+            }),
+          ),
+          const SizedBox(height: 10),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Agree',
+                    style: TextStyle(
+                      color: Color(0xFF37843A),
+                      fontSize: 12,
+                    )),
+                Text('Disagree',
+                    style: TextStyle(
+                      color: Color(0xFFA32E25),
+                      fontSize: 12,
+                    )),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          // Divider
+          Divider(
+            thickness: 0.5,
+            color: Colors.grey[400],
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
     );
   }
 }
--- lib/pages/questionnaire.dart
+++ lib/pages/questionnaire.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:baidyet/components/likert_scale_question.dart';
 import 'package:baidyet/components/my_button.dart';
-import 'package:flutter/material.dart';
 
 class QuestionnaireWidget extends StatefulWidget {
   const QuestionnaireWidget({Key? key}) : super(key: key);
@@ -10,91 +10,186 @@ class QuestionnaireWidget extends StatefulWidget {
 }
 
 class _QuestionnaireWidgetState extends State<QuestionnaireWidget> {
-  int _currentQuestionIndex = 0;
-  late List<int?> _answers;
+  int _currentPage = 0;
+  late List<List<int?>> _answers;
+  String? _errorMessage;
+  bool _showQuestionnaire = false;
 
-  final List<String> questions = [
-    "Question No. 1. Question No. 1. Question No. 1.",
-    "Question No. 2. Question No. 2. Question No. 2.",
-    // Add more questions as needed
+  final List<List<String>> questionPages = [
+    List.generate(
+        10, (index) => "Question ${index + 1}. This is question ${index + 1}."),
+    List.generate(10,
+        (index) => "Question ${index + 11}. This is question ${index + 11}."),
+    // Add more pages of 10 questions as needed
   ];
 
   @override
   void initState() {
     super.initState();
-    _answers = List.filled(questions.length, null);
+    _answers = List.generate(
+      questionPages.length,
+      (index) => List.filled(questionPages[index].length, null),
+    );
+    WidgetsBinding.instance.addPostFrameCallback((_) => _showInitialDialog());
   }
 
-  void _selectAnswer(int value) {
+  void _showInitialDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Assessment Required"),
+          content: const Text(
+            "We need to assess you so that the AI can create a personal budget plan tailored just for you. "
+            "Please make sure you are confident with your answers, as you cannot return to previous pages once you move forward.",
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("Start Assessment"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() {
+                  _showQuestionnaire = true;
+                });
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _selectAnswer(int questionIndex, int value) {
     setState(() {
-      _answers[_currentQuestionIndex] = value;
+      _answers[_currentPage][questionIndex] = value;
+      _errorMessage = null; // Clear error message when an answer is selected
     });
   }
 
-  void _nextQuestion() {
-    if (_currentQuestionIndex < questions.length - 1) {
-      setState(() {
-        _currentQuestionIndex++;
-      });
+  bool _areAllQuestionsAnswered() {
+    return _answers[_currentPage].every((answer) => answer != null);
+  }
+
+  void _nextPage() {
+    if (_areAllQuestionsAnswered()) {
+      if (_currentPage < questionPages.length - 1) {
+        setState(() {
+          _currentPage++;
+          _errorMessage = null;
+        });
+      } else {
+        _onComplete();
+      }
     } else {
-      _onComplete();
+      setState(() {
+        _errorMessage = "Please answer all questions before proceeding.";
+      });
     }
   }
 
   void _onComplete() {
-    List<int> finalAnswers = _answers.whereType<int>().toList();
     // Handle the completed questionnaire
-    print('Answers: $finalAnswers');
+    print('All answers: $_answers');
     // You can add more logic here, such as navigating to a new page or showing a completion dialog
+  }
+
+  double _calculateProgress() {
+    int totalQuestions = questionPages.flatten().length;
+    int answeredQuestions =
+        _answers.flatten().where((answer) => answer != null).length;
+    return answeredQuestions / totalQuestions;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: const Color(0xFFF0EAE2),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+    return SafeArea(
+      child: Stack(
         children: [
-          // Progress bar
-          LinearProgressIndicator(
-            value: (_currentQuestionIndex + 1) / questions.length,
-            backgroundColor: Colors.grey[300],
-            valueColor: const AlwaysStoppedAnimation<Color>(Colors.blue),
-          ),
-          const SizedBox(height: 24),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF3F4F6),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Progress bar
+                LinearProgressIndicator(
+                  value: _calculateProgress(),
+                  backgroundColor: Colors.grey[300],
+                  valueColor: const AlwaysStoppedAnimation(Color(0xFF34ACB7)),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                const SizedBox(height: 30),
 
-          // Questions
-          Expanded(
-            child: ListView.builder(
-              itemCount: questions.length,
-              itemBuilder: (context, index) {
-                return Opacity(
-                  opacity: index == _currentQuestionIndex ? 1.0 : 0.5,
-                  child: LikertScaleQuestion(
-                    question: questions[index],
-                    selectedValue: _answers[index],
-                    onSelect:
-                        index == _currentQuestionIndex ? _selectAnswer : (_) {},
+                // Questions and Next button
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: questionPages[_currentPage].length +
+                        1, // +1 for the Next button
+                    itemBuilder: (context, index) {
+                      if (index < questionPages[_currentPage].length) {
+                        // Question
+                        bool isAnswered = _answers[_currentPage][index] != null;
+                        bool isFirstUnanswered = !isAnswered &&
+                            _answers[_currentPage]
+                                .take(index)
+                                .every((answer) => answer != null);
+                        double opacity =
+                            isAnswered || isFirstUnanswered ? 1.0 : 0.5;
+
+                        return LikertScaleQuestion(
+                          question: questionPages[_currentPage][index],
+                          selectedValue: _answers[_currentPage][index],
+                          onSelect: (value) => _selectAnswer(index, value),
+                          isEnabled: true,
+                          opacity: opacity,
+                        );
+                      } else {
+                        // Next button
+                        return Column(
+                          children: [
+                            const SizedBox(height: 20),
+                            if (_errorMessage != null)
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: Text(
+                                  _errorMessage!,
+                                  style: const TextStyle(
+                                      color: Colors.red, fontSize: 14),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            MyButton(
+                              onTap: _nextPage,
+                              buttonName:
+                                  _currentPage < questionPages.length - 1
+                                      ? "Next →"
+                                      : "Complete",
+                            ),
+                          ],
+                        );
+                      }
+                    },
                   ),
-                );
-              },
+                ),
+              ],
             ),
           ),
-
-          const SizedBox(height: 24),
-
-          // Next button
-          MyButton(
-            onTap:
-                _answers[_currentQuestionIndex] != null ? _nextQuestion : null,
-            buttonName: "Next →",
-          ),
+          if (!_showQuestionnaire)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: const Center(child: CircularProgressIndicator()),
+            ),
         ],
       ),
     );
   }
+}
+
+// Extension method to flatten nested lists
+extension ListFlatten<T> on List<List<T>> {
+  List<T> flatten() => expand((element) => element).toList();
 }
